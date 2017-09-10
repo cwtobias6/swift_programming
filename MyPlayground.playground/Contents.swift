be4656c3b6b68e022d0f2cfa7c34dd8f179da2a9
Christian Tobias <christiantobias@Christians-MacBook-Pro.local>
2017-09-10 09:49:53 -0700

    hello world

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground programmer"
+
+str += "!!"
+
+print(str)
+
+var tish = "Mr.Tish"
+
+print("Hello " + tish)
