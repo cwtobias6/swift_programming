01ca08f4a971bf5044a800855769aff8d30a32a4
Christian Tobias <christiantobias@Christians-MacBook-Pro.local>
2017-09-11 17:02:54 -0700

    Finished Getting Started Section

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+let townName = "Knowhere"
+var unemployed: Int = 67
+
+let townDescription = "\(townName) has a population of \(population), and \(unemployed) people unemployed, and \(numberOfStoplights) stoplights."
+
+
+print(townDescription)
+
