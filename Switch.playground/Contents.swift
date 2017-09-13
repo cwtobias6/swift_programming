31dd5d0d2996cacfa903c06f79e1d3a13c0ac28a
Christian Tobias <christiantobias@Christians-MacBook-Pro.local>
2017-09-12 22:12:51 -0700

    More Switch and Tuples learning

--- Switch.playground/Contents.swift
+++ Switch.playground/Contents.swift
@@ -2,5 +2,48 @@
 
 import Cocoa
 
-var statusCode: Int = 404
+var statusCode: Int = 418
 var errorString: String = "The request failed with the error:"
+
+
+switch statusCode {
+case 100, 101:
+    errorString += " Informational, \(statusCode)."
+    
+case 204:
+    errorString += " Successful but no content, 204."
+    
+case 300...307:
+    errorString += " Redirection, \(statusCode)."
+    
+case 400...417:
+    errorString += " Client error, \(statusCode)."
+    
+case 500...505:
+    errorString += " Server error, \(statusCode)."
+    
+case let unknownCode where (unknownCode >= 200 && unknownCode < 300)
+    || unknownCode > 505:
+    errorString = "\(unknownCode) is not a known error code."
+    
+default:
+    errorString = "Unexpected error encountered."
+}
+
+let error = (code: statusCode, error: errorString)
+
+let firstErrorCode = 404
+let secondErrorCode = 200
+let errorCodes = (firstErrorCode, secondErrorCode)
+
+switch errorCodes {
+case (404, 404):
+    print("No items found.")
+case (404, _):
+    print("First item not found.")
+case (_, 404):
+    print("Second item not found.")
+default:
+    print("All items found.")
+}
+
