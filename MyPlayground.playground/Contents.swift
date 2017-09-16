c45014ebd00f6c53438e14c26bd66b8d00507590
Christian Tobias <christiantobias@Christians-MacBook-Pro.local>
2017-09-16 10:38:20 -0700

    Swift Basics

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,13 +1,20 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
+import UIKit
 
-var str = "Hello, playground programmer"
+var str = "Hello, playground"
 
-str += "!!"
+var hello = "hello"; var playground = "playground"
+
+for i in 0..<64 {
+    var point = sin(Double(i) * 100)
+}
+
 
 print(str)
 
-var tish = "Mr.Tish"
+NSLog(str)
 
-print("Hello " + tish)
+print(hello, playground, separator: "_", terminator: "$$$")
+
+print("\n\(hello), and Welcome")
