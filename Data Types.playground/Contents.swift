28c45d93355fa7962110fcd7b4438a2110b2711e
Christian Tobias <christiantobias@Christians-MacBook-Pro.local>
2017-09-16 11:28:51 -0700

    Optionals and Int Data Types

---
+++ Data Types.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let double_number: Double = 1.123456789012345 // Double: 15 Decimal point precision
+
+
+var dubs = Double(34) * double_number
+
+dubs.rounded()
+
+let be_true = true
+
+let alwaysLie: Bool = false
+
+typealias TF = Bool
+
+let hungry: TF = true
+
+print(hungry)
+
+var wordsOfWisdom: String
+
+wordsOfWisdom = "Do or do not, there is no try"
+
+let constantString: String
+
+constantString = "Don't wish. DO."
+
+var maybeAString: String?
+
+print(maybeAString)
+
+maybeAString = "Luck favors the prepared."
+
+print(maybeAString)
+
+print(maybeAString!) // force unwrap an optional
+
+
