9a55841d76d8c0966c107f72ae587c42b63ff848
Christian Tobias <christiantobias@Christians-MacBook-Pro.local>
2017-09-17 15:29:17 -0700

    Arrays and Dictionaries

---
+++ stings arrays and tuples.playground/Contents.swift
@@ -0,0 +1,36 @@
+
+
+let letterA: Character = "\u{61}"
+
+let quote = "In the end, we only regret the chances we didn't take"
+
+// Strings are passed by copy
+
+var newQuote = quote
+
+newQuote = "In the end, it's not the years in your life that count. It's the life in your years."
+
+
+
+print(quote)
+print("This is the new quote: \(newQuote)")
+
+// Arrays
+
+var threeSttoges = ["Moe", "Larry", "Curly"]
+
+var inningScores: [Int]
+
+inningScores = []
+
+threeSttoges.count
+
+// Dictionaries 
+
+var nicknames = ["Gamertag": "Lil Scarface 09", "Georgetown Name": "Tish"]
+
+nicknames["Gamertag"]
+
+//Sets
+
+var teachers = Set<String>()
