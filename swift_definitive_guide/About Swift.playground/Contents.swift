6b74a3faeeb9cb480e53415367fb380d249f2373
Christian Tobias <cwtobias6@gmail.com>
2017-09-18 13:27:16 -0700

    Finished opening section

--- swift_definitive_guide/About Swift.playground/Contents.swift
+++ swift_definitive_guide/About Swift.playground/Contents.swift
@@ -178,9 +178,35 @@ miniCircle.area()
 miniCircle.simpleDescription()
 
 
+// Enumerations and Structures 
 
+enum Rank: Int {
+    
+    case ace = 1
+    case two,three,four,five,six,seven,eight,nine,ten
+    case jack, queen, king
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "Jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "King"
+        default:
+            return String(self.rawValue)
+            
+        }
+    }
+    
+}
 
+let ace = Rank.ace
 
+let aceRawValue = ace.rawValue
 
 
 
