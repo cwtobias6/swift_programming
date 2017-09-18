8da847ac722536d51b2ac7cd38d6a856f6c6c414
Christian Tobias <cwtobias6@gmail.com>
2017-09-18 09:11:47 -0700

    Functions

--- swift_definitive_guide/About Swift.playground/Contents.swift
+++ swift_definitive_guide/About Swift.playground/Contents.swift
@@ -62,6 +62,50 @@ default:
 
 // Functions and Closures
 
+func greet(person: String, day: String) -> String {
+    
+    return "Hello \(person), today is \(day)."
+}
+
+greet(person: "Tish" , day: "Monday")
+
+func favFood(_ food: String) -> String { // Underscore allows for optional argument Label
+    return "I love to eat \(food)."
+}
+
+favFood("Chicken Wings")
+
+func sumOf(numbers: Int...) -> Int {
+    var sum = 0
+    
+    for number in numbers {
+        sum += number
+    }
+    
+    return sum
+}
+
+sumOf()
+
+sumOf(numbers: 3,44,65)
+
+func avgOf(numbers: Int...) -> Int {
+    var total = 0
+    
+    for number in numbers {
+        total += number
+    }
+    
+    return total / numbers.count
+    
+}
+
+avgOf(numbers: 10,20)
+
+
+
+
+
 
 
 
