5f3671a84bc2dd5b046e622ba8ee902422857bdc
Christian Tobias <cwtobias6@gmail.com>
2017-09-19 17:57:56 -0700

    Dictionary Start

---
+++ big_nerd_ranch/Dictionaries.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var movieRatings = ["Donnie Darko": 4, "Chungking Express": 5, "Dark City": 4]
+print("I have rated \(movieRatings.count) movies.")
+
+let darkoRating = movieRatings["Donnie Darko"]
+
