cf98d8913c766c4b9a7bd51ec45a4d14a9105c25
Christian Tobias <cwtobias6@gmail.com>
2017-09-19 17:29:13 -0700

    Arrays and array methods

--- big_nerd_ranch/Arrays.playground/Contents.swift
+++ big_nerd_ranch/Arrays.playground/Contents.swift
@@ -1,13 +1,48 @@
-//: Playground - noun: a place where people can play
-
 import Cocoa
 
-var bucketList = ["Climb Mt.Everest"]
+var bucketList = ["Climb Mt. Everest"]
+var newItems = [
+    "Fly hot air balloon to Fiji",
+    "Watch the Lord of the Rings trilogy in one day",
+    "Go on a walkabout",
+    "Scuba dive in the Great Blue Hole",
+    "Find a triple rainbow"
+]
+bucketList += newItems
+bucketList
+bucketList.remove(at: 2)
+print(bucketList.count)
+print(bucketList[0...2])
+bucketList[2] += " in Australia"
+bucketList[0] = "Climb Mt. Kilimanjaro"
+bucketList.insert("Toboggan across Alaska", at: 2)
+bucketList
 
-bucketList.append("Chicken Wing")
-bucketList.append("Watch the Lord of the Rings trilogy in one day")
-bucketList.append("Go on a walkabout")
-bucketList.append("Scuba dive in the Great Blue Hole")
-bucketList.append("Find a triple rainbow")
+var myronsList = [
+    "Climb Mt. Kilimanjaro",
+    "Fly hot air balloon to Fiji",
+    "Toboggan across Alaska",
+    "Go on a walkabout in Australia",
+    "Find a triple rainbow",
+    "Scuba dive in the Great Blue Hole",
+    "Find a triple rainbow"
+]
 
-bucket
+let equal = (bucketList == myronsList)
+
+///////
+
+let lunches = [
+    "Cheeseburger",
+    "Veggie Pizza",
+    "Chicken Caesar Salad",
+    "Black Bean Burrito",
+    "Falafel Wrap"
+]
+
+var toDoList = ["Take out garbage", "Pay bills", "Cross off finished items"]
+
+toDoList.isEmpty
+var reversedList = Array(toDoList.reversed())
+print(reversedList)
+
