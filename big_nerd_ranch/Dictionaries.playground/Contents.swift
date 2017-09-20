fa495ebbeb8c99fc44f6b8c1bf87d615a13aa7f9
Christian Tobias <cwtobias6@gmail.com>
2017-09-20 09:21:56 -0700

    Finished Dictionaries

--- big_nerd_ranch/Dictionaries.playground/Contents.swift
+++ big_nerd_ranch/Dictionaries.playground/Contents.swift
@@ -7,3 +7,30 @@ print("I have rated \(movieRatings.count) movies.")
 
 let darkoRating = movieRatings["Donnie Darko"]
 
+movieRatings["Dark City"] = 5
+movieRatings
+
+let oldRating: Int? = movieRatings.updateValue(5, forKey: "Donnie Darko")
+
+if let lastRating = oldRating, let currentRating = movieRatings["Donnie Darko"] {
+    print("Old rating: \(lastRating); current rating: \(currentRating)")
+}
+
+movieRatings["The Cabinet of Dr. Caligari"] = 5
+movieRatings.removeValue(forKey: "Dark City") // or movieRatings["Dark City"] = nil
+
+for (key,value) in movieRatings {
+    print("The key is: \(key) | The value is: \(value)")
+}
+
+for movie in movieRatings.keys {
+    print("User has rated \(movie).")
+}
+
+let album = ["Diet Roast Beef": 268,
+             "Dubba Dubbs Stubs His Toe": 467,
+             "Smokey's Carpet Cleaning Service": 187,
+             "Track 4": 221]
+
+// Transform a Dictionay into an Array
+let watchedMovies = Array(movieRatings.keys)
