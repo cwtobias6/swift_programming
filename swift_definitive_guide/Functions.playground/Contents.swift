2f12d7d0701acd763e8293353272b7e060087648
Christian Tobias <cwtobias6@gmail.com>
2017-09-26 16:36:13 -0700

    functions progress

--- swift_definitive_guide/Functions.playground/Contents.swift
+++ swift_definitive_guide/Functions.playground/Contents.swift
@@ -16,3 +16,20 @@ func printAndCount(string:String) -> Int {
 }
 
 printAndCount(string: "Chicken Wings")
+
+// Functions with multiple returns, return tuples
+
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+minMax(array: [1,2,3,4,5,6,7])
