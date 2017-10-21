2a342513ae88fdd392ef2b86d42fd95ee637f314
Christian Tobias <cwtobias6@gmail.com>
2017-10-21 16:37:12 -0700

    Dictionaries

---
+++ swift_definitive_guide/Dictionaries.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//A dictionary stores associations between keys of the same type and values of the same type in a collection with no defined ordering. Each value is associated with a unique key, which acts as an identifier for that value within the dictionary.
+
+
