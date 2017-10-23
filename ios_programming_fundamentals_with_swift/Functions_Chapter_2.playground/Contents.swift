2d5458744a1330d4aeac7a79f1e2e90d8e80c803
Christian Tobias <cwtobias6@gmail.com>
2017-10-23 14:38:48 -0700

    Functions Progress

--- ios_programming_fundamentals_with_swift/Functions_Chapter_2.playground/Contents.swift
+++ ios_programming_fundamentals_with_swift/Functions_Chapter_2.playground/Contents.swift
@@ -8,10 +8,42 @@ func echoString(_ s:String, times:Int) -> String {
     return result
 }
 
-let s = echoString("hi ", times:3)
+let stringy = echoString("hi ", times:3)
 
 // Function overloading
 func say (_ what:String) {
 }
 func say (_ what:Int) {
 }
+
+// In Out Parameters
+
+/* In the function body, we removed all occurrences of the character from the local copy of the String parameter, but this change didn’t affect the original string. */
+
+func removeCharacter(_ c:Character, from s:String) -> Int {
+    var s = s
+    var howMany = 0
+    while let ix = s.index(of:c) {
+        s.remove(at:ix)
+        howMany += 1
+    }
+    return howMany
+}
+
+let s = "hello"
+let result = removeCharacter("l", from:s)
+
+
+// But now we can change the variable outside of the function with InOut Parameter
+func removeCharacter(_ c:Character, from s: inout String) -> Int {
+    var howMany = 0
+    while let ix = s.index(of:c) {
+        s.remove(at:ix)
+        howMany += 1
+    }
+    return howMany
+}
+
+var hello = "hello"
+let result2 = removeCharacter("l", from:&hello)
+print(hello) // prints "heo"
