e33094ea6701ea9cf3295c071daaddba254488cc
Christian Tobias <cwtobias6@gmail.com>
2017-10-30 14:17:42 -0700

    Enumerations Chapter Completed

--- big_nerd_ranch/Enumerations.playground/Contents.swift
+++ big_nerd_ranch/Enumerations.playground/Contents.swift
@@ -83,16 +83,98 @@ enum Lightbulb {
             return ambient
         }
     }
+    
+    mutating func toggle() {
+        switch self {
+        case .on:
+            self = .off
+        case .off:
+            self = .on
+        }
+    }
 }
 
+var bulb = Lightbulb.on
+
+bulb.toggle() //off
+
+bulb.toggle() // back on
+
+var bulbTemp = bulb.surfaceTemperature(forAmbientTemperature: 77)
 
 
+print(bulbTemp)
 
 
+// Associated values allow you to attach data to instances of an enumeration, and different cases can have different types of associated values.
 
 
+enum ShapeDimensions {
+    
+    // This is Deimensionless
+    case point
+    
+    // square's associated value is the length of one side
+    case square(side: Double)
+    
+    // rectangle's associated value defines its width and height
+    case rectangle(width: Double, height: Double)
+    
+    func area() -> Double {
+        switch self {
+        
+        case .point:
+            return 0
+        case let .square(side: side):
+            return side * side
+            
+        case let .rectangle(width: w, height: h):
+            return w * h
+        }
+    }
+    
+    func perimeter() -> Double {
+        
+        switch self {
+            case .point:
+                return 0
+        case let .square(side:side):
+                return side * 4
+        case let .rectangle(width: w, height: h):
+                return (w*2) + (h*2)
+        }
+        
+        
+    }
+    
+}
+
+var squareShape = ShapeDimensions.square(side: 10.0)
+var rectShape = ShapeDimensions.rectangle(width: 5.0, height: 10.0)
+var pointShape = ShapeDimensions.point
+
+print("square's area = \(squareShape.area())")
+print("rectangle's area = \(rectShape.area())")
+print("point's area = \(pointShape.area())")
 
 
+// Recursive Enumerations, Tree Data Structures
+
+/* Swift can introduce a layer of indirection. Instead of deciding how much memory oneKnownParent will require (which would lead back into infinite recursion), you can use the keyword indirect to instruct the compiler to instead store the enum’s data behind a pointer. */
+
+indirect enum FamilyTree {
+    case noKnownParents
+    case oneKnownParent(name: String, ancestors: FamilyTree)
+    case twoKnownParents(fatherName: String, fatherAncestors: FamilyTree,
+        motherName: String, motherAncestors: FamilyTree)
+}
+
+// Example
+let fredAncestors = FamilyTree.twoKnownParents(
+    fatherName: "Fred Sr.",
+    fatherAncestors: .oneKnownParent(name: "Beth", ancestors: .noKnownParents),
+    motherName: "Marsha",
+    motherAncestors: .noKnownParents)
 
 
 
