ee64670389c195112c5c9553746d1c9541f75c4b
Christian Tobias <cwtobias6@gmail.com>
2017-10-31 09:16:40 -0700

    Init MonsterTown: a story of Classes and Structs

---
+++ big_nerd_ranch/MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Christian Tobias on 10/31/17.
+//  Copyright © 2017 Christian Tobias. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
