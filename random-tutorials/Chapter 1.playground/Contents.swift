2eb6c2b2dcee895ded620e36cc73829bc40c678d
Christian Tobias <cwtobias6@gmail.com>
2017-11-16 15:05:01 -0800

    Chapter 1 completed

---
+++ random-tutorials/Chapter 1.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+// Basic Print Statement
+print("Hello, Swift Apprentice reader!")
+
+
+// Integer vs. Double
+22 / 7
+22.0 / 7.0
+
+// Truncating remainders with Modulos
+28 % 10
+28.0.truncatingRemainder(dividingBy: 10.0)
+
+
+// Shif Operations
+
+1 << 3
+
+1 << 4
+
+2 << 3
+
+8 << 1
+
+32 >> 1
+
+// Challenges
+
+var exercises = 9
+var exercisesSolved = 0
+exercisesSolved += 1
+
+
+
+
+
+
+
+
+
+
+
+
