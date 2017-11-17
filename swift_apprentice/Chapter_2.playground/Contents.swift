5ba71a8e3b35e5da022bc2f3d0d92f0cd0270395
Christian Tobias <cwtobias6@gmail.com>
2017-11-16 16:44:43 -0800

    String and Number progress

--- swift_apprentice/Chapter_2.playground/Contents.swift
+++ swift_apprentice/Chapter_2.playground/Contents.swift
@@ -2,4 +2,31 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Type conversion
+
+var integer: Int = 22
+var double: Double = 25.23
+
+var doubleConvertedToInteger = Int(double) // loss of precision
+
+var wannabeDouble = 4.0
+
+wannabeDouble + 1
+
+// Tuples
+
+var aRandomTuple = (2,4)
+
+aRandomTuple.0
+aRandomTuple.1
+
+
+let coordinates3D = (x: 2, y: 3, z: 1)
+coordinates3D.x
+let (x3, y3, z3) = coordinates3D
+
+z3
+
+
+
+
