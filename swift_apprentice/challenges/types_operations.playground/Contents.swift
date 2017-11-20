3dd0f005e4a37a0423ea989bc0e0e524a329e00e
Christian Tobias <cwtobias6@gmail.com>
2017-11-20 11:20:12 -0800

    Finish challeneges for chapter 2

---
+++ swift_apprentice/challenges/types_operations.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// #1
+let coordinate = (2,3)
+
+// #2
+
+var row:Int = coordinate.0
+var column:Int = coordinate.1
+
+let namedCoordinate = (row, column)
+
+
+// #3
+
+// Answered on page
+
+// # 4
+
+let tuple = (day: 15, month: 8, year: 2015)
+let day = tuple.day
+
+// #5
+
+// Attempting to alter the value of a constant
+
+// #6, #7, #8, #9answered on the page
+
+// #10
+
+Double.pi
+Float.pi
+
+
+
