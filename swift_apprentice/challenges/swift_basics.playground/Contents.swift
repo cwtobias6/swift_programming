bf6e70bd5be184ea52b9aa2eeeb11643a758c377
Christian Tobias <cwtobias6@gmail.com>
2017-11-20 09:57:00 -0800

    Finish Challenges for chapter 1

---
+++ swift_apprentice/challenges/swift_basics.playground/Contents.swift
@@ -0,0 +1,94 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// #1
+let exercises = 9
+var exercisesSolved = 0
+
+exercisesSolved += 1
+
+// #2
+
+var age = 16
+print(age)
+age = 30
+print(age)
+
+exercisesSolved += 1
+
+// #3
+
+let a: Int = 46
+let b: Int = 10
+
+let answer1: Int = (a * 100) + b
+let answer2: Int = (a * 100) + (b * 100)
+let answer3: Int = (a * 100) + (b / 10)
+
+exercisesSolved += 1
+
+// #4
+
+(5*3)-((4/2)*2)
+
+exercisesSolved += 1
+
+// #5
+
+var number1 = 4.0
+var number2 = 3.0
+
+var average = (number1 + number2) / 2
+
+exercisesSolved += 1
+
+// #6
+
+var fahrenheit: Double = 58
+
+var celcius: Double = (fahrenheit - 32) / 1.8
+
+exercisesSolved += 1
+
+// #7
+
+var position: Int = 23
+
+var row = position / 8
+var column = position % 8
+
+exercisesSolved += 1
+
+// #8
+
+let degrees: Double = 360
+
+var radians = degrees / (2 * Double.pi)
+
+exercisesSolved += 1
+
+// #9
+
+let x1: Double = 1.0
+let y1: Double = 3.0
+let x2: Double = 4.0
+let y2: Double = 8.0
+let dx: Double = x2 - x1
+let dy: Double = y2 - y1
+let distance: Double = (dx * dx + dy * dy).squareRoot()
+exercisesSolved += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
